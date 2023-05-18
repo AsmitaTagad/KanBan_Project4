9db39a567a5341e8ca04f094d62cfe370aad71e5
AsmitaTagad <asmitatagad07@gmail.com>
2023-05-18 21:25:51 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import DialogBox from "./Components/Dialog/DialogBox";
+import Home from "./home/Home";
+import { RecoilRoot } from "recoil";
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={
+          <>
+            <Home />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="/details/:id" element={<DialogBox />} />
+      </Route>
+    )
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
   );
 }
 
